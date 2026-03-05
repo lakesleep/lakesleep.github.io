54efc2b6acaad83aeb0d36d5d2343789ca629e31
lakesleep <147892488+lakesleep@users.noreply.github.com>
2026-03-05 03:43:01 -0400

    Update script.js

--- blog/script.js
+++ blog/script.js
@@ -66,7 +66,7 @@ let headerHTML = '<ul> <li><a href="' + relativePath + '/blogindex.html">blog ho
 
 //Generate the Footer HTML, which uses the variables defined in the BASIC INFO section above to list info about the site.
 //Note: feel free to remove the references to Zonelets and Neocities! Just be careful not to delete any necessary HTML closing tags or other syntax.
-let footerHTML = "<hr><p><img src='images/water_dividers_transparent_3.png' style='object-fit: scale-down;'/></p>";
+let footerHTML = "<hr style='height:1px;color:white;'><p><img src='images/water_dividers_transparent_3.png' style='object-fit: scale-down;'/></p>";
 
 //To do the following stuff, we want to know where we are in the posts array (if we're currently on a post page).
 let currentIndex = -1;
