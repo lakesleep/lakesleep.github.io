670eb892053cfd17fb020a84b4bce6f395032b34
lakesleep <147892488+lakesleep@users.noreply.github.com>
2026-03-23 16:51:34 -0400

    Update script.js

--- blog/script.js
+++ blog/script.js
@@ -34,8 +34,8 @@ let postsArray = [
 //[ "posts/2020-11-10-Special-Characters-Example.html", encodeURI( 'Spéci@l "Character\'s" Examp|e' ) ],
 //[ "posts/2020-11-10-My-Third-Post-Example.html" ],
 //[ "posts/2020-11-10-My-Second-Post-Example.html" ],
-[ "posts/2026-03-23-glittering-metals.html" ]
-[ "posts/2026-03-05-relating-to-the-infinite.html" ];
+[ "posts/2026-03-23-glittering-metals.html" ],
+[ "posts/2026-03-05-relating-to-the-infinite.html" ] ];
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
